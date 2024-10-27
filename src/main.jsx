ec0ba729ecb2af28c207a6b25ddde0480c30066f
Kody <157696416+kydoCode@users.noreply.github.com>
2024-10-27 23:54:04 +0100

    Index.css to root

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './i18n';
-import './styles/index.css';
+import '../index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
