890082b84407935a95a3c9a514fe3dee40cb3923
Kody <kydokody@gmail.com>
2024-10-28 10:29:18 +0100

    Fixed CSS setting and dependencies issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './i18n';
-import '../index.css';
+import './i18n'
+import './index.css'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { ChevronRight, Globe } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import ProjectModal from '../components/ProjectModal'
-import reposData from '../data/repos_github_for_portfolio.json';
+import reposData from '../data/repos_github_for_portfolio.json'
 
 export default function Portfolio() {
   const { t, i18n } = useTranslation()
@@ -136,7 +136,7 @@ export default function Portfolio() {
                     <button
                       onClick={() => openModal(project)}
                       className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    >
+    >
                       {t('latestProjects.viewDetails')}
                     </button>
                   </div>
