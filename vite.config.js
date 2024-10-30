189bcee3b5e2a41a402478ffd5547a691018531c
Kydo <157696416+kydoCode@users.noreply.github.com>
2024-10-30 09:56:47 +0100

    Fix build configuration for Vite
    Update `vite.config.js` and `public/index.html` to fix build process.
    
    * Remove `root: 'public'` configuration from `vite.config.js`.
    * Change the script tag in `public/index.html` to reference `/src/main.jsx` instead of `./src/main.jsx`.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'public',
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
