e28816d24c3f10701b6d98766cd0bdd6bada4716
Kydo <157696416+kydoCode@users.noreply.github.com>
2024-10-31 16:37:10 +0100

    Update App.jsx
    Edit BrowserRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Portfolio from './pages/Portfolio'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
