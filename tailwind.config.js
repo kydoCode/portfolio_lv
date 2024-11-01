f259509fbc8caa34ed68d3e6a85eb626a9ebadc7
Kody <kydokody@gmail.com>
2024-11-01 23:04:57 +0100

    Revert "Merge pull request #48 from kydoCode/fix-education-jsx"
    This reverts commit 92348bf3b6c756fad15611fcbe7c29a78a68874a, reversing
    changes made to ea2fc0988b6d36e703ba774a1c02e5f6ec180363.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,39 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      width: {
-        'left-img': '100%',
-        'right-img': '100%',
-      },
-      maxWidth: {
-        'left-img': '150px',
-        'right-img': '300px',
-      },
-      height: {
-        'left-img': 'auto',
-        'right-img': 'auto',
-      },
-      borderRadius: {
-        'left-img': '50%',
-        'right-img': '8px',
-      },
-      borderWidth: {
-        'left-img': '2px',
-        'right-img': '2px',
-      },
-      padding: {
-        'left-img': '5px',
-      },
-      objectFit: {
-        'left-img': 'cover',
-        'right-img': 'cover',
-      },
-      borderColor: {
-        'left-img': '#ddd',
-        'right-img': '#ddd',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
