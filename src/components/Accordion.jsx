76c09a173b52f2b4a8fdec632ea44be9d4272bab
Kody <157696416+kydoCode@users.noreply.github.com>
2024-11-21 19:15:03 +0100

    Cancle destination path modification in vercel.json

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,6 @@
 import { Accordion as RadixAccordion, AccordionItem, AccordionTrigger, AccordionContent } from '@radix-ui/react-accordion';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 import '../index.css';
-// import { Html5, Css3, Javascript, ReactLogo, Tailwind, Bootstrap, Php, Mysql, Nodejs, Express, Vuejs, Python, Postgresql, Nosql, Swift, Swiftui, Kanban, Scrum, Trello, Vscode, Xcode, Github, Copilot, Chatgpt, Linters, Figma } from 'lucide-react'
 
 const Accordion = ({ items }) => {
   return (
