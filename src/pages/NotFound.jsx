903de4834e67cf7bef61b9923870fcd3d1447813
Kody <157696416+kydoCode@users.noreply.github.com>
2025-01-14 12:08:45 +0100

    Add tailwind to h1 and p2 as well as header and footer componentsto NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function NotFound() {
   return (
     <div>
+      <Header />
       <h1>404 - Page not found</h1>
       <p>Désolé, la page que vous cherchez n'existe pas.</p>
+      <Footer />
     </div>
   );
 }
