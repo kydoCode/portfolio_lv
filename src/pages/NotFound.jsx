815c9e95f8cd528317cbcd2e391b88b1381a40e5
Kody <157696416+kydoCode@users.noreply.github.com>
2025-01-14 12:09:47 +0100

    Add last modificaitons to 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,8 +5,8 @@ function NotFound() {
   return (
     <div>
       <Header />
-      <h1>404 - Page not found</h1>
-      <p>Désolé, la page que vous cherchez n'existe pas.</p>
+      <h1 className="text-3xl font-bold mt-4 mb-4">404 - Page not found</h1>
+      <p className="mt-1 text-sm text-gray-500">Désolé, la page que vous cherchez n'existe pas.</p>
       <Footer />
     </div>
   );
