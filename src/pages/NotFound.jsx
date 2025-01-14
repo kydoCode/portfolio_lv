cf6e37a46cf40aa16333dd187da24c35c55b94d9
Kody <157696416+kydoCode@users.noreply.github.com>
2025-01-14 12:14:58 +0100

    Correct framgents in NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 function NotFound() {
   return (
     <div>
-      <Header />
-      <h1 className="text-3xl font-bold mt-4 mb-4">404 - Page not found</h1>
-      <p className="mt-1 text-sm text-gray-500">Désolé, la page que vous cherchez n'existe pas.</p>
-      <Footer />
+        <Header />
+        <div>
+          <h1 className="text-3xl font-bold mt-4 mb-4">404 - Page not found</h1>
+          <p className="mt-1 text-sm text-gray-500">Désolé, la page que vous cherchez n'existe pas.</p>
+        </div>
+        <Footer />
     </div>
   );
 }
